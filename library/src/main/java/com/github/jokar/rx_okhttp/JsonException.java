f3af409c67e4be988e4781c9e3ab32f070fd54cd
JokAr <dongqi.gui@taojiji.com>
2019-07-29 10:20:37 +0800

    增加获取解析失败的json

--- library/src/main/java/com/github/jokar/rx_okhttp/JsonException.java
+++ library/src/main/java/com/github/jokar/rx_okhttp/JsonException.java
@@ -4,9 +4,10 @@ package com.github.jokar.rx_okhttp;
  * Create by JokAr. on 2019-06-04.
  */
 public class JsonException extends RuntimeException {
-
+    private String json;
     public JsonException(String json) {
         super("can't analysis json for string \n" + json);
+        this.json = json;
     }
 
     public JsonException(String json, RuntimeException e) {
@@ -17,5 +18,9 @@ public class JsonException extends RuntimeException {
     public JsonException(String json, Exception e) {
         super("can't analysis json for string \n" + json + "\n"
                 + e.getMessage());
+    }
+
+    public String getJson() {
+        return json;
     }
 }
